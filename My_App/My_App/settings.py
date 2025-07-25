cb916bad21213aa515ce5277aead59f6daf58561
Ruslan Yarmak <ruslan.yarmak.intech@gmail.com>
2025-07-25 17:00:13 +0300

    Update settings

--- My_App/My_App/settings.py
+++ My_App/My_App/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#skecm5d%+w#tp!vq$m#sjcm6mzl&y*&&kt7qn1_b4y+&^+-!j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
